570edbabb18643b46a0eb1d3171f5738d81e57d2
Ali Raeini <a.q.raeini@gmail.com>
2025-08-11 21:33:01 +0100

    limit test run to two processes

--- test2f/voxcylCFMesh.sh
+++ test2f/voxcylCFMesh.sh
@@ -16,7 +16,7 @@ printf "DimSize = 22 32 32 \n\
 export RefineLevel=0.90  #< this should be less than one for cfMesh to work, check!
 export endTime=0.001 #< consider increasing this to 0.01
 export UD1s=1000
-export nProc=4
+export nProc=2  # The GitHub Action are limited to two procs only
 export oilFilldFracs=0.5
 rm -rf voxcyl32cfm*/ AllRunImageTwoPhaseCFMesh
 AllRunImageTwoPhaseCFMesh voxcyl32cfm.mhd  # this just copies AllRunImageTwoPhase
